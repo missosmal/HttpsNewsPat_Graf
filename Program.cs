f3e75ab58a49e5b394140bd20c039f1577d3afb3
lewan4ik <student-a502@PC-A502-20>
2024-12-23 10:51:18 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpsNewsPat_Graf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
